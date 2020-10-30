5cf38143973d778c7207b92be1f8f9816b62996c
Prince Ikuto Phoenix <33163837+IkutoPhoenix@users.noreply.github.com>
2020-10-30 21:35:16 +0100

    Base Plugin & gitignore update

---
+++ plugins/mc2vox/VoxelWitch/src/com/particubes/witch/Witch.java
@@ -0,0 +1,16 @@
+package com.particubes.witch;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Witch extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
