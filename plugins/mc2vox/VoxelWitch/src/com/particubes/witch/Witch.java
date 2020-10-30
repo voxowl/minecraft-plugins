133fe7e5a6073acb744703cd81e87c0c5c19c364
Prince Ikuto Phoenix <33163837+IkutoPhoenix@users.noreply.github.com>
2020-10-30 22:20:39 +0100

    onEnable WorldEdit check, base onCommand

--- plugins/mc2vox/VoxelWitch/src/com/particubes/witch/Witch.java
+++ plugins/mc2vox/VoxelWitch/src/com/particubes/witch/Witch.java
@@ -4,9 +4,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Witch extends JavaPlugin {
 
+    public static Witch instance;
+    boolean WorldEditAPI = false;
+
     @Override
     public void onEnable() {
-
+        instance = this;
+        if (getServer().getPluginManager().getPlugin("WorldEdit") != null) {
+            getLogger().info("WorldEdit found.");
+            WorldEditAPI = true;
+        } else {
+            getLogger().info("WorldEdit not found. Disabling VoxelWitch...");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+        getLogger().info("VoxelWitch is now active ! Check out our Voxel Game : https://particubes.com ! :)");
     }
 
     @Override
---
+++ plugins/mc2vox/VoxelWitch/src/com/particubes/witch/commandVoxelWitch.java
@@ -0,0 +1,14 @@
+package com.particubes.witch;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class commandVoxelWitch implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        return false;
+    }
+}
